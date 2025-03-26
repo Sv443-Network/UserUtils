3a5d4864d4ca986aeb0e231fa9ac84f89d7b2595
Sv443 <contact@sv443.net>
2025-03-26 23:15:10 +0100

    feat: create coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      enabled: true,
+      provider: "v8",
+      include: ["lib/**/*.ts"],
+      reporter: ["text", "text-summary", "html", "lcov"],
+    },
     include: ["lib/**/*.spec.ts"],
     environment: "jsdom",
     testTimeout: 10_000,
