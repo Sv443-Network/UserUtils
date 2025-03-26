4de7967a362223d7263d0ec82e56426152d321c5
Sv443 <contact@sv443.net>
2025-03-26 23:46:52 +0100

    fix: remove some files from coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
       enabled: true,
       provider: "v8",
       include: ["lib/**/*.ts"],
+      exclude: [
+        "lib/**/*.spec.ts",
+        "lib/Dialog.ts", // DOM-only features can't be tested for now cause they aren't rendered
+        "lib/SelectorObserver.ts", // ^
+        "lib/index.ts",
+        "lib/types.ts",
+      ],
       reporter: ["text", "text-summary", "html", "lcov"],
     },
     include: ["lib/**/*.spec.ts"],
