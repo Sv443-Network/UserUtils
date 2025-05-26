1250bf2bc1a7273256e6a7ec583c5876d51db11c
Sv443 <contact@sv443.net>
2025-05-26 02:13:45 +0200

    chore: change to alpha version

--- lib/index.ts
+++ lib/index.ts
@@ -3,9 +3,10 @@
  * UserUtils is a lightweight library with various utilities for userscripts, allowing you to register listeners for when CSS selectors exist, intercept events, create persistent & synchronous data stores, modify the DOM more easily and much more
  */
 
+export * from "@sv443-network/coreutils";
+
 export * from "./Dialog.js";
 export * from "./dom.js";
 export * from "./Errors.js";
 export * from "./Mixins.js";
 export * from "./SelectorObserver.js";
-export * from "@sv443-network/coreutils";
