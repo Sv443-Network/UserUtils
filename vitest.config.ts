7f45c16d9ae26842c9eb1938ea2ce1b706aedb24
Sv443 <contact@sv443.net>
2026-02-08 03:13:33 +0100

    ref: minor fixes

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         "lib/dom.ts",              // partially tested, but since some features can just never be tested, exclude the file from report
         "lib/Dialog.ts",           // DOM-only features can't be tested for now cause they aren't properly rendered, just mounted into a pseudo-DOM
         "lib/SelectorObserver.ts", // same here ^
-        "lib/errors.ts",
+        "lib/Errors.ts",
         "lib/index.ts",
       ],
       reporter: ["text", "text-summary", "lcov"],
